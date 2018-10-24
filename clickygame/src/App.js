29f2d03975b1e6d60018cff444b93a579c9ceded
Trey Marak <treymarak@Alfreds-MacBook-Pro.local>
2018-10-23 19:43:14 -0500

    added better style

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     this.state.cards.forEach(card => {
       card.count = 0;
     });
-    alert(`Game Over :( \nscore: ${this.state.score}`);
+    alert(`Game Over! Try again for a better score! \nscore: ${this.state.score}`);
     this.setState({score: 0});
     return true;
   }
